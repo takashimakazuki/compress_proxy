9929abdc3097378e56a3c2b9f7ab9d1af3b2c744
takashimakazuki <sora3takashima@gmail.com>
2024-02-02 00:51:16 +0900

    delete unused file

--- host/test_sendrecv.c
+++ host/test_sendrecv.c
@@ -20,23 +20,35 @@ int main(int argc, char **argv)
     }
 
 
-    int len = 10000;
+    int len = 1*1024*1024; // 1MB
     char src_buf[len+1];
     char dst_buf[len+1];
     
+    memset(src_buf, 'A', len);
+    src_buf[len] = '\0';
+
+    double t_start, t_end;
 
     for(int iter=0; iter<1; iter++) {
         if (rank == 0) {
-            memset(src_buf, 'A', len);
-            src_buf[len] = '\0';
+            t_start = MPI_Wtime();
+            
             MPI_Send(src_buf, len, MPI_CHAR, 1, 0xbeef, MPI_COMM_WORLD);
-            printf("Iter%d, rank%d Send finished '%.8s'\n", iter, rank, src_buf);
+
+            MPI_Recv(dst_buf, len, MPI_CHAR, 1, 0xbeef, MPI_COMM_WORLD, NULL);
+            t_end = MPI_Wtime();
         } else {
             MPI_Recv(dst_buf, len, MPI_CHAR, 0, 0xbeef, MPI_COMM_WORLD, NULL);
-            printf("Iter%d, rank%d Receive finished '%.8s'\n", iter, rank, dst_buf);
+            MPI_Send(src_buf, len, MPI_CHAR, 0, 0xbeef, MPI_COMM_WORLD);
         }
         MPI_Barrier(MPI_COMM_WORLD);
-        printf("==========Barrier==========\n");
+        if (rank == 0) {
+            printf("==========Barrier==========\n");
+            printf("Iter%d finished\n", iter);
+            double latency = (t_end - t_start) * 1e6;
+            printf("ping-pong latency/ latency: %f us / %f us\n", latency, latency/2);
+            printf("ping-pong latency/ latency: %f ms / %f ms\n", latency/1000, latency/1000/2);
+        }
     }
 
     MPI_Finalize();
